d120f81d82e15a5a374c86b260838b62905d6436
Hex <wolfgunmiller@gmail.com>
2018-05-21 22:47:47 +0300

    L012
     : L010

--- app/src/main/java/com/test/hex/draftapp/MainActivity.kt
+++ app/src/main/java/com/test/hex/draftapp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.test.hex.draftapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
 import android.view.View
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.TextView
+import android.widget.*
+
+private const val TAG = "myLogs"
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,7 +20,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        useLayout(R.layout.l011_resources)
+        useLayout(R.layout.l010_click_buttons)
     }
 
     private fun useLayout(layout: Int){
@@ -57,26 +59,60 @@ class MainActivity : AppCompatActivity() {
                 textView = findViewById(R.id.textView)
                 textView.text = getString(R.string.click_any_button)
 
+                Log.d(TAG, "Найдем три кнопки")
+
                 button1 = findViewById(R.id.button1)
+                Log.d(TAG, "Найдена кнопка 1")
                 button2 = findViewById(R.id.button2)
+                Log.d(TAG, "Найдена кнопка 2")
                 button3 = findViewById(R.id.button3)
+                Log.d(TAG, "Найдена кнопка 3")
 
                 button1.setOnClickListener {
                     textView.text = getString(R.string.text1)
+                    Log.d(TAG, "Нажата кнопка 1")
+                    Toast.makeText(this, "Нажата кнопка 1",
+                            Toast.LENGTH_SHORT)
+                            .centerGravitation()
+                            .show()
                 }
+
                 button2.setOnClickListener {
                     textView.text = getString(R.string.text2)
+                    Log.d(TAG, "Нажата кнопка 2")
+                    Toast.makeText(this, "Нажата кнопка 2",
+                            Toast.LENGTH_SHORT)
+                            .centerGravitation()
+                            .show()
                 }
                 button3.setOnClickListener {
                     textView.text = getString(R.string.text3)
+                    Log.d(TAG, "Нажата кнопка 3")
+                    Toast.makeText(this, "Нажата кнопка 3",
+                            Toast.LENGTH_SHORT)
+                            .centerGravitation()
+                            .show()
                 }
                 textView.setOnClickListener {
                     textView.text = getString(R.string.click_any_button)
-                    button3.text = getString(R.string.button_number_three)
+//                    button3.text = getString(R.string.button_number_three)
+                    val toast = Toast.makeText(this, "Нажат текст",
+                            Toast.LENGTH_LONG)
+                    toast.setGravity(Gravity.TOP, 0, 0)
+                    val toastImage = toast.view as LinearLayout
+                    val imageView = ImageView(this)
+                    imageView.setImageResource(R.drawable.image001)
+                    toastImage.addView(imageView, 0)
+                    toast.show()
                 }
             }
 
             else -> setContentView(layout)
         }
     }
+}
+
+private fun Toast.centerGravitation(): Toast {
+    this.setGravity(Gravity.CENTER, 0, 0)
+    return this
 }
