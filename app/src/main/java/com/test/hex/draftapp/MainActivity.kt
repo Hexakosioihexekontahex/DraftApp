614555563cdeaa6ba544e8e2746b0b97c6d272e2
Hex <wolfgunmiller@gmail.com>
2018-05-21 21:58:49 +0300

    L011

--- app/src/main/java/com/test/hex/draftapp/MainActivity.kt
+++ app/src/main/java/com/test/hex/draftapp/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        useLayout(R.layout.l010_click_buttons)
+        useLayout(R.layout.l011_resources)
     }
 
     private fun useLayout(layout: Int){
